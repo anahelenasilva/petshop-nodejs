9db7d2276dd2720d5e956e0c64429c5b475cc0a4
Ana Helena <anahelenarp@hotmail.com>
2021-05-08 09:42:55 -0300

    Add address, credit-card e pet models

---
+++ src/backoffice/models/credit-card.model.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+export class CreditCard {
+    constructor(
+        public holder: string,
+        public number: string,
+        public expiration: string
+    ) {
+
+    }
+}
