8dad9b1a5a9dcf0c1fdb7a5dfbeaf8354df41188
Ana Helena <anahelenarp@hotmail.com>
2021-05-11 13:45:09 -0300

    Add schemas

--- src/backoffice/contracts/customer.contracts.ts
+++ src/backoffice/contracts/customer.contracts.ts
@@ -13,7 +13,7 @@ export class CreateCustomerContract implements IContract {
         flunt.hasMinLen(model.name, 5, 'Nome inválido');
         flunt.isEmail(model.email, 'Email inválido');
         flunt.isFixedLen(model.document, 11, 'CPF inválido');
-        flunt.hasMinLen(model.password, 6, 'Senha inválida');
+        // flunt.hasMinLen(model.password, 6, 'Senha inválida');
 
         this.errors = flunt.errors;
         return flunt.isValid();
