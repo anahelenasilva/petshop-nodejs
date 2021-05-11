7a851043af0eef9702d940db209fcf7216a9099a
Ana Helena <anahelenarp@hotmail.com>
2021-05-11 18:20:57 -0300

    Criando um pet

--- src/backoffice/controllers/customer.controller.ts
+++ src/backoffice/controllers/customer.controller.ts
@@ -3,9 +3,11 @@ import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Post, 
 import { ValidatorInterceptor } from 'src/interceptors/validator.interceptor';
 import { CreateAddressContract } from '../contracts/customer/create-address.contract';
 import { CreateCustomerContract } from '../contracts/customer/create-customer.contract';
+import { CreatePetContract } from '../contracts/customer/create-pet.contract';
 import { CreateCustomerDto } from '../dtos/create-customer.dto';
 import { Address } from '../models/address.model';
 import { Customer } from '../models/customer.model';
+import { Pet } from '../models/pet.model';
 import { Result } from '../models/result.model';
 import { User } from '../models/user.model';
 import { AccountService } from '../services/account.service';
@@ -34,7 +36,7 @@ export class CustomerController {
         try {
             const user = await this.accountService.create(new User(model.document, model.password, true));
 
-            var customer = new Customer(model.name, model.document, model.email, null, null, null, null, user);
+            var customer = new Customer(model.name, model.document, model.email, [], null, null, null, user);
             const createdCustomer = await this.customerService.create(customer);
 
             return new Result('Cliente criado com sucesso', true, { user: user, customer: createdCustomer }, null);
@@ -65,6 +67,18 @@ export class CustomerController {
         } catch (error) {
             //rollback manual
             throw new HttpException(new Result('Erro ao cadastrar o endereço de entrega', false, null, error), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @Post(':document/pets')
+    @UseInterceptors(new ValidatorInterceptor(new CreatePetContract()))
+    async addPet(@Param('document') document, @Body() model: Pet) {
+        try {
+            await this.customerService.createPet(document, model);
+            return new Result('Pet criado com sucesso', true, model, null);
+        } catch (error) {
+            //rollback manual
+            throw new HttpException(new Result('Erro ao cadastrar o pet', false, null, error), HttpStatus.BAD_REQUEST);
         }
     }
 
--- src/backoffice/services/customer.service.ts
+++ src/backoffice/services/customer.service.ts
@@ -3,6 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Address } from '../models/address.model';
 import { Customer } from '../models/customer.model';
+import { Pet } from '../models/pet.model';
 
 @Injectable()
 export class CustomerService {
@@ -28,6 +29,15 @@ export class CustomerService {
         return await this.model.findOneAndUpdate({ document }, {
             $set: {
                 shippingAddress: data,
+            }
+        }, options);
+    }
+
+    async createPet(document: string, data: Pet): Promise<Customer> {
+        const options = { upsert: true, new: true };
+        return await this.model.findOneAndUpdate({ document }, {
+            $push: {
+                pets: data,
             }
         }, options);
     }
