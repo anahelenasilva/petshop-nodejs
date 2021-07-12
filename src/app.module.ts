1805813313466babf28eaf9db9cb8a124db31247
Ana Helena <anahelenarp@hotmail.com>
2021-07-12 09:02:54 -0300

    Refatoração

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { BackofficeModule } from './backoffice/backoffice.module';
+import { BackofficeModule } from 'src/backoffice/backoffice.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://ana:ana123@cluster0.yzgzr.azure.mongodb.net/petdb?retryWrites=true&w=majority'),
+    MongooseModule.forRoot(
+      'mongodb+srv://ana:ana123@cluster0.yzgzr.azure.mongodb.net/petdb?retryWrites=true&w=majority',
+    ),
     BackofficeModule,
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule { }
+export class AppModule {}
