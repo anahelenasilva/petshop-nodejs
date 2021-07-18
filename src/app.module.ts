667d815289fa2513a7be15ebe9e5c51ca268d735
Ana Helena <anahelenarp@hotmail.com>
2021-07-18 17:34:56 -0300

    Add mysql config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { BackofficeModule } from 'src/modules/backoffice/backoffice.module'
 import { StoreModule } from 'src/modules/store/store.module'
 
@@ -8,6 +9,16 @@ import { StoreModule } from 'src/modules/store/store.module'
     MongooseModule.forRoot(
       'mongodb+srv://ana:ana123@cluster0.yzgzr.azure.mongodb.net/petdb?retryWrites=true&w=majority'
     ),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: 'ana123',
+      database: 'petshop',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true
+    }),
     BackofficeModule,
     StoreModule
   ],
