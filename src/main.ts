0eeaf781671a34cd3f7fd38d7bb5b2722268a96c
Ana Helena <anahelenarp@hotmail.com>
2021-07-25 17:09:58 -0300

    Add logger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from 'src/app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from 'src/app.module'
+import { CustomLogger } from './shared/services/logger.service'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    logger: new CustomLogger()
+  })
+  await app.listen(3000)
 }
-bootstrap();
+bootstrap()
