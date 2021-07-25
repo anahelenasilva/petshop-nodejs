ba7dfab31b03b9b9abd301707c40c9b8d7d48930
Ana Helena <anahelenarp@hotmail.com>
2021-07-25 17:11:57 -0300

    Compression

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { NestFactory } from '@nestjs/core'
+import * as compression from 'compression'
+
 import { AppModule } from 'src/app.module'
 import { CustomLogger } from './shared/services/logger.service'
 
@@ -6,6 +8,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: new CustomLogger()
   })
+
+  app.use(compression())
   await app.listen(3000)
 }
 bootstrap()
