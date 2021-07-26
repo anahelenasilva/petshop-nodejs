8094b7ccf45cd6473c4609d063f8eeb21d1f2ae7
Ana Helena <anahelenarp@hotmail.com>
2021-07-26 10:00:12 -0300

    Resolvendo dependências

--- src/agenda/agenda.module.ts
+++ src/agenda/agenda.module.ts
@@ -1,4 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common'
+import { CqrsModule } from '@nestjs/cqrs'
+import { CommandHandlers } from './commands/handlers'
+import { AgendaController } from './controller/agenda.controller'
+import { RoomRepository } from './repositories/room.repository'
+import { RoomBookService } from './services/room-book.service'
 
-@Module({})
+@Module({
+  imports: [CqrsModule],
+  controllers: [AgendaController],
+  providers: [RoomBookService, RoomRepository, ...CommandHandlers]
+})
 export class AgendaModule {}
