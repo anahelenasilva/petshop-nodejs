e1ffdd96f6d9b8466f8ece2dd71275c2347a875f
Ana Helena <anahelenarp@hotmail.com>
2021-07-26 10:08:20 -0300

    Chamando evento

--- src/agenda/agenda.module.ts
+++ src/agenda/agenda.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common'
 import { CqrsModule } from '@nestjs/cqrs'
+
 import { CommandHandlers } from './commands/handlers'
 import { AgendaController } from './controller/agenda.controller'
+import { EventHandlers } from './events/handlers'
 import { RoomRepository } from './repositories/room.repository'
 import { RoomBookService } from './services/room-book.service'
 
 @Module({
   imports: [CqrsModule],
   controllers: [AgendaController],
-  providers: [RoomBookService, RoomRepository, ...CommandHandlers]
+  providers: [
+    RoomBookService,
+    RoomRepository,
+    ...CommandHandlers,
+    ...EventHandlers
+  ]
 })
 export class AgendaModule {}
--- src/agenda/models/room.model.ts
+++ src/agenda/models/room.model.ts
@@ -1,4 +1,5 @@
 import { AggregateRoot } from '@nestjs/cqrs'
+import { RoomBookedEvent } from '../events/room-booked.event'
 
 export class Room extends AggregateRoot {
   constructor(private readonly id: string) {
@@ -7,5 +8,7 @@ export class Room extends AggregateRoot {
 
   book(customerId: string) {
     //regras de negócio
+
+    this.apply(new RoomBookedEvent(customerId, this.id))
   }
 }
